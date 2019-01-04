ea5e2953cea901f9f8792e7679fdc6a18eb0aa92
KaylinOwO <freec990@gmail.com>
2019-01-04 15:45:39 -0500

    thing

--- Panels.cpp
+++ Panels.cpp
@@ -68,19 +68,12 @@ void __fastcall Hooked_PaintTraverse( PVOID pPanels, int edx, unsigned int vguiP
 
 			if (gMisc.purebypass.value || gESP.player_enabled.value)
 			{
-				if (!gInts.Engine->IsInGame())
+				void** pure_addr = nullptr;
+				if (!pure_addr) 
 				{
-					void *pure_orig = nullptr;
-					void **pure_addr = nullptr;
-
-					if (!pure_addr)
-					{
-						pure_addr = *reinterpret_cast<void ***>(gSignatures.GetEngineSignature("A1 ? ? ? ? 56 33 F6 85 C0") + 1); //Signature from F12017 github
-					}
-					if (*pure_addr)
-						pure_orig = *pure_addr;
-					*pure_addr = (void *)0;
-				}
+					pure_addr = *reinterpret_cast<void***>(gSignatures.GetEngineSignature("A1 ? ? ? ? 56 33 F6 85 C0") + 1);
+				} *
+				pure_addr = (void*)0;
 			}
 
 			int iWidth, iHeight; //Resolution fix, so this can work in Fullscreen
